9312ce9f113e3c308f02aab5be064ba1c4642f62
advanderveer <advanderveer@gmail.com>
2016-12-08 21:11:23 +0100

    benchmarked random read, decrypt and read

--- split_test.go
+++ split_test.go
@@ -6,7 +6,6 @@ import (
 	"crypto/cipher"
 	"crypto/sha1"
 	"crypto/sha256"
-	"encoding/binary"
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -66,14 +65,22 @@ func BenchmarkRawChunking(b *testing.B) {
 	}
 }
 
+var boltSize int64
+var boltPath string
+var boltKeys [][32]byte
+var boltInput []byte
+var boltOutput []byte
+
 func BenchmarkConcurrentChunkingSha2Bolt(b *testing.B) {
-	size := int64(1024 * 1024 * 1024)
-	r := bytes.NewReader(randb(size))
+	boltSize = int64(1024 * 1024 * 1024)
+	boltInput = randb(boltSize)
+	boltOutput = make([]byte, len(boltInput))
+	r := bytes.NewReader(boltInput)
 	chnkr := chunker.New(r, secret.Pol())
 	buf := make([]byte, chunker.MaxSize)
 	dir, _ := ioutil.TempDir("", "libchunk")
-	path := filepath.Join(dir, "db.bolt")
-	db, _ := bolt.Open(path, 0777, nil)
+	boltPath = filepath.Join(dir, "db.bolt")
+	db, _ := bolt.Open(boltPath, 0777, nil)
 	block, err := aes.NewCipher(secret[:])
 	if err != nil {
 		b.Fatalf("failed to create AES block cipher: %v", err)
@@ -85,10 +92,9 @@ func BenchmarkConcurrentChunkingSha2Bolt(b *testing.B) {
 	}
 
 	defer db.Close()
-	defer os.Remove(path)
 
 	b.ResetTimer()
-	b.SetBytes(int64(size))
+	b.SetBytes(int64(boltSize))
 	for i := 0; i < b.N; i++ {
 		r.Seek(0, 0)
 		chnkr.Reset(r, secret.Pol())
@@ -107,11 +113,15 @@ func BenchmarkConcurrentChunkingSha2Bolt(b *testing.B) {
 				wg.Add(1)
 				go func() {
 					defer wg.Done()
-					encrypted := gcm.Seal(chunk.Data[:0], k[:], chunk.Data, nil)
+					encrypted := gcm.Seal(nil, k[:], chunk.Data, nil)
+					if bytes.Equal(encrypted, chunk.Data) {
+						b.Fatal("huh")
+					}
 
 					wg.Add(1)
 					go func() {
 						defer wg.Done()
+
 						err = db.Batch(func(tx *bolt.Tx) error {
 							b, _ := tx.CreateBucketIfNotExists([]byte("a"))
 							return b.Put(k[:], encrypted)
@@ -120,14 +130,187 @@ func BenchmarkConcurrentChunkingSha2Bolt(b *testing.B) {
 						if err != nil {
 							b.Fatal(err)
 						}
+
+						boltKeys = append(boltKeys, k)
 					}()
-
 				}()
-
 			}()
 		}
 
 		wg.Wait()
+	}
+}
+
+func BenchmarkConcurrentChunkingSha2FromBolt(b *testing.B) {
+	db, _ := bolt.Open(boltPath, 0777, nil)
+	block, err := aes.NewCipher(secret[:])
+	if err != nil {
+		b.Fatalf("failed to create AES block cipher: %v", err)
+	}
+
+	gcm, err := cipher.NewGCMWithNonceSize(block, sha256.Size)
+	if err != nil {
+		b.Fatalf("failed to setup GCM cipher mode: %v", err)
+	}
+
+	defer db.Close()
+
+	b.ResetTimer()
+	b.SetBytes(int64(boltSize))
+	for i := 0; i < b.N; i++ {
+		outf, _ := ioutil.TempFile("", "libchunk_")
+		defer outf.Close()
+
+		for _, k := range boltKeys {
+			var chunk []byte
+			err = db.View(func(tx *bolt.Tx) error {
+				b := tx.Bucket([]byte("a"))
+				v := b.Get(k[:])
+				if v == nil || len(v) < 1 {
+					return fmt.Errorf("not found")
+				}
+
+				chunk = make([]byte, len(v))
+				copy(chunk, v)
+				return nil
+			})
+
+			if err != nil {
+				b.Fatal(err)
+			}
+
+			plaintext, err := gcm.Open(nil, k[:], chunk, nil)
+			if err != nil {
+				b.Fatal(err)
+			}
+
+			//@TODO benchmark output to file: benchmarked at 700Mb/s
+			//@TODO benchmark push to s3, goal: 1GB/s
+			_, err = outf.Write(plaintext)
+			if err != nil {
+				b.Fatal(err)
+			}
+		}
+	}
+}
+
+var diskDir string
+var diskKeys [][32]byte
+var diskSize int64
+
+func BenchmarkConcurrentChunkingSha2Disk(b *testing.B) {
+	diskSize = int64(1024 * 1024 * 1024)
+	r := bytes.NewReader(randb(diskSize))
+	chnkr := chunker.New(r, secret.Pol())
+	buf := make([]byte, chunker.MaxSize)
+	diskDir, _ = ioutil.TempDir("", "libchunk")
+	block, err := aes.NewCipher(secret[:])
+	if err != nil {
+		b.Fatalf("failed to create AES block cipher: %v", err)
+	}
+
+	gcm, err := cipher.NewGCMWithNonceSize(block, sha256.Size)
+	if err != nil {
+		b.Fatalf("failed to setup GCM cipher mode: %v", err)
+	}
+
+	b.ResetTimer()
+	b.SetBytes(int64(diskSize))
+	for i := 0; i < b.N; i++ {
+		r.Seek(0, 0)
+		chnkr.Reset(r, secret.Pol())
+		wg := &sync.WaitGroup{}
+		for {
+			chunk, err := chnkr.Next(buf)
+			if err == io.EOF {
+				break
+			}
+
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				k := sha256.Sum256(chunk.Data)
+
+				wg.Add(1)
+				go func() {
+					defer wg.Done()
+					encrypted := gcm.Seal(nil, k[:], chunk.Data, nil)
+
+					wg.Add(1)
+					go func() {
+						defer wg.Done()
+
+						f, err := os.OpenFile(filepath.Join(diskDir, fmt.Sprintf("%x", k)), os.O_CREATE|os.O_RDWR|os.O_EXCL, 0777)
+						if err != nil {
+							if os.IsExist(err) {
+								return
+							}
+
+							b.Fatal(err)
+						}
+
+						defer f.Close()
+						_, err = f.Write(encrypted)
+						if err != nil {
+							b.Fatal(err)
+						}
+
+						diskKeys = append(diskKeys, k)
+					}()
+				}()
+			}()
+		}
+
+		wg.Wait()
+	}
+}
+
+func BenchmarkConcurrentChunkingSha2FromFS(b *testing.B) {
+	db, _ := bolt.Open(boltPath, 0777, nil)
+	block, err := aes.NewCipher(secret[:])
+	if err != nil {
+		b.Fatalf("failed to create AES block cipher: %v", err)
+	}
+
+	gcm, err := cipher.NewGCMWithNonceSize(block, sha256.Size)
+	if err != nil {
+		b.Fatalf("failed to setup GCM cipher mode: %v", err)
+	}
+
+	defer db.Close()
+
+	b.ResetTimer()
+	b.SetBytes(int64(boltSize))
+	for i := 0; i < b.N; i++ {
+		outf, _ := ioutil.TempFile("", "libchunk_")
+		defer outf.Close()
+
+		for _, k := range diskKeys {
+			kpath := filepath.Join(diskDir, fmt.Sprintf("%x", k))
+
+			func() {
+				f, err := os.OpenFile(kpath, os.O_RDONLY, 0777)
+				if err != nil {
+					b.Fatal(err)
+				}
+
+				defer f.Close()
+				chunk, err := ioutil.ReadAll(f)
+				if err != nil {
+					b.Fatal(err)
+				}
+
+				plaintext, err := gcm.Open(nil, k[:], chunk, nil)
+				if err != nil {
+					b.Fatal(err)
+				}
+
+				_, err = outf.Write(plaintext)
+				if err != nil {
+					b.Fatal(err)
+				}
+			}()
+		}
 	}
 }
 
@@ -180,30 +363,6 @@ func BenchmarkRawSha2Concurrent4MBChunks(b *testing.B) {
 				ioutil.Discard.Write(k[:])
 			}()
 		}
-	}
-}
-
-func BenchmarkRawGCM(b *testing.B) {
-	size := int64(64 * 1024 * 1024)
-	data := randb(size)
-
-	block, err := aes.NewCipher(secret[:])
-	if err != nil {
-		b.Fatalf("failed to create AES block cipher: %v", err)
-	}
-
-	gcm, err := cipher.NewGCMWithNonceSize(block, sha1.Size)
-	if err != nil {
-		b.Fatalf("failed to setup GCM cipher mode: %v", err)
-	}
-
-	b.ResetTimer()
-	b.SetBytes(int64(size))
-	for i := 0; i < b.N; i++ {
-		buf := make([]byte, gcm.NonceSize())
-		binary.PutVarint(buf, int64(i))
-		encrypted := gcm.Seal(data[:0], buf, data, nil)
-		ioutil.Discard.Write([]byte(encrypted))
 	}
 }
 
@@ -524,44 +683,44 @@ func tempDB(t quiter) (p string, db *bolt.DB) {
 // // 	benchBufSplit(b, buf)
 // // }
 //
-// func TestSplitJoinSmallNonRandom(t *testing.T) {
-// 	_, db := tempDB(t)
-// 	input := []byte("foo bar") //@TODO find somall content that generates multiple chunks
-// 	output := bytes.NewBuffer(nil)
-//
-// 	pr, pw := io.Pipe()
-// 	go func() {
-// 		defer pw.Close()
-// 		err := libchunk.Split(db, secret, bytes.NewBuffer(input), pw)
-// 		if err != nil {
-// 			t.Fatalf("failed to split: %v", err)
-// 		}
-// 	}()
-//
-// 	err := libchunk.Join(db, secret, pr, output)
-// 	if err != nil {
-// 		t.Fatalf("failed to join: %v", err)
-// 	}
-//
-// 	if !bytes.Equal(input, output.Bytes()) {
-// 		t.Errorf("expected joined output (len %d) to be the same as input (len %d)", output.Len(), len(input))
-// 	}
-//
-// 	//assert encryption at rest
-// 	//assert throughput
-//
-// 	//assert:
-// 	//  - assert writer output
-// 	//  - different polynomials generate different
-// 	//    - key
-// 	//    - encrypted content
-// 	//  - different secret parts generate random noise
-// 	//  -assert multiple chunks being outputed
-//
-// 	//assert db file size
-// 	//assert buffer content
-// 	//assert chunks in db
-// 	//assert deduplication
-// 	//assert encryption
-//
-// }
+func TestSplitJoinSmallNonRandom(t *testing.T) {
+	_, db := tempDB(t)
+	input := []byte("foo bar") //@TODO find somall content that generates multiple chunks
+	output := bytes.NewBuffer(nil)
+
+	pr, pw := io.Pipe()
+	go func() {
+		defer pw.Close()
+		err := libchunk.Split(db, secret, bytes.NewBuffer(input), pw)
+		if err != nil {
+			t.Fatalf("failed to split: %v", err)
+		}
+	}()
+
+	err := libchunk.Join(db, secret, pr, output)
+	if err != nil {
+		t.Fatalf("failed to join: %v", err)
+	}
+
+	if !bytes.Equal(input, output.Bytes()) {
+		t.Errorf("expected joined output (len %d) to be the same as input (len %d)", output.Len(), len(input))
+	}
+
+	//assert encryption at rest
+	//assert throughput
+
+	//assert:
+	//  - assert writer output
+	//  - different polynomials generate different
+	//    - key
+	//    - encrypted content
+	//  - different secret parts generate random noise
+	//  -assert multiple chunks being outputed
+
+	//assert db file size
+	//assert buffer content
+	//assert chunks in db
+	//assert deduplication
+	//assert encryption
+
+}
