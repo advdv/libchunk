4e8fd1b01ff78f0bf796e172e27f0e0f0783e6ed
advanderveer <advanderveer@gmail.com>
2016-12-19 10:04:21 +0100

    refactored join for concurrent work

--- join.go
+++ join.go
@@ -6,36 +6,85 @@ import (
 	"os"
 )
 
-//Join will read and decrypt chunks for keys provided through the iterator
-//and writes chunk contents to writer 'w' in order of key appearance.
+//Join will read and decrypt chunks for keys provided by the iterator and writes
+//each chunk's contents to writer 'w' in order of key appearance. Chunks
+//are fetched concurrently (locally or remote) but are guaranteed arrive in
+//order to writer 'w' for assembly in the original format
 func Join(keys KeyIterator, w io.Writer, conf Config) error {
-	for {
-		k, err := keys.Next()
+
+	//result of working the item
+	type result struct {
+		chunk []byte
+		err   error
+	}
+
+	//work item
+	type item struct {
+		key   K
+		resCh chan *result
+		err   error
+	}
+
+	//work is run concurrently
+	work := func(it *item) {
+		chunk, err := conf.Store.Get(it.key)
 		if err != nil {
-			if err == io.EOF {
+			if os.IsNotExist(err) {
+				//@TODO add fetching from remote
+				it.resCh <- &result{nil, ErrNoSuchKey}
+				return
+			}
+
+			it.resCh <- &result{nil, fmt.Errorf("failed to find key '%s': %v", it.key, err)}
+			return
+		}
+
+		res := &result{}
+		res.chunk, res.err = conf.AEAD.Open(nil, it.key[:], chunk, nil)
+		it.resCh <- res
+	}
+
+	//fan-out concurrent work
+	itemCh := make(chan *item, 10)
+	go func() {
+		defer close(itemCh)
+		for {
+			k, err := keys.Next()
+			if err != nil {
+				if err != io.EOF {
+					itemCh <- &item{
+						key: k,
+						err: fmt.Errorf("failed to iterate into next key: %v", err),
+					}
+				}
+
 				break
 			}
 
-			return fmt.Errorf("failed to iterate into next key: %v", err)
-		}
-
-		chunk, err := conf.Store.Get(k)
-		if err != nil {
-			if os.IsNotExist(err) {
-				return ErrNoSuchKey
+			it := &item{
+				key:   k,
+				resCh: make(chan *result),
 			}
 
-			return fmt.Errorf("failed to find key '%s': %v", k, err)
+			go work(it)  //create work
+			itemCh <- it //send to fan-in thread for syncing results
+		}
+	}()
+
+	//fan in, output plaintext chunks
+	for it := range itemCh {
+		if it.err != nil {
+			return fmt.Errorf("failed to iterate: %v", it.err)
 		}
 
-		plaintext, err := conf.AEAD.Open(nil, k[:], chunk, nil)
-		if err != nil {
-			return fmt.Errorf("failed to decrypt chunk '%s': %v", k, err)
-		}
-
-		_, err = w.Write(plaintext)
-		if err != nil {
-			return fmt.Errorf("failed to write chunk '%s' to output: %v", k, err)
+		res := <-it.resCh
+		if res.err != nil {
+			return fmt.Errorf("failed to work chunk '%s': %v", it.key, res.err)
+		} else {
+			_, err := w.Write(res.chunk)
+			if err != nil {
+				return fmt.Errorf("failed to write chunk '%s' to output: %v", it.key, err)
+			}
 		}
 	}
 
